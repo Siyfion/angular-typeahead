a858302d188734433f4f6a1d9ffcc1c2e90ffa69
Simon Mansfield <siyfion@gmail.com>
2013-11-15 10:15:32 +0000

    REV: Updated to 0.0.7 FEAT: Adds binding to "input" event, which then updates the ngModel with the raw user input. BUG: Stops the watch on ngModel being called from an internal watch (this wasn't needed).

--- angular-typeahead.js
+++ angular-typeahead.js
@@ -7,12 +7,14 @@ angular.module('siyfion.sfTypeahead', [])
         ngModel: '='
       },
       link: function (scope, element) {
+        var localChange = false;
         element.typeahead(scope.datasets);
 
         // Updates the ngModel binding when a value is manually selected from the dropdown.
         // ToDo: Think about how the value could be updated on user entry...
         element.bind('typeahead:selected', function (object, datum, dataset) {
           scope.$apply(function() {
+            localChange = true;
             scope.ngModel = datum;
             scope.selectedDataset = dataset;
           });
@@ -21,29 +23,43 @@ angular.module('siyfion.sfTypeahead', [])
         // Updates the ngModel binding when a query is autocompleted.
         element.bind('typeahead:autocompleted', function (object, datum, dataset) {
           scope.$apply(function() {
+            localChange = true;
             scope.ngModel = datum;
             scope.selectedDataset = dataset;
           });
         });
 
+        // Updates the ngModel binding when the user enters some text
+        element.bind('input', function () {
+          scope.$apply(function () {
+            localChange = true;
+            var value = element.val();
+            scope.ngModel = value;
+          });
+        });
+
         // Updates typeahead when ngModel changed.
         scope.$watch('ngModel', function (newVal) {
-            if ($.isArray(scope.datasets)) {
-                for (var i=0;i<scope.datasets.length;i++) {
-                    if (scope.datasets[i].name ==  scope.selectedDataset) {
-                        var valueKey = scope.datasets[i].valueKey;
-                        break;
-                    }
-                }
+          if (localChange) {
+            localChange = false;
+            return;
+          }
 
-            } else {
-                var valueKey = scope.datasets.valueKey;
+          if ($.isArray(scope.datasets)) {
+            for (var i = 0; i < scope.datasets.length; i++) {
+              if (scope.datasets[i].name ==  scope.selectedDataset) {
+                var valueKey = scope.datasets[i].valueKey;
+                break;
+              }
             }
+          } else {
+            var valueKey = scope.datasets.valueKey;
+          }
 
-            if (newVal && valueKey && newVal.hasOwnProperty(valueKey)) {
-                newVal = newVal[valueKey];
-            }
-            element.typeahead('setQuery', newVal || '');
+          if (newVal && valueKey && newVal.hasOwnProperty(valueKey)) {
+            newVal = newVal[valueKey];
+          }
+          element.typeahead('setQuery', newVal || '');
         });
       }
     };
